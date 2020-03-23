1d1aeed75d01e82fd1f829cddd05950d7530947d
patrickclery <patrick.clery@gmail.com>
2020-03-23 09:29:39 -0700

    :white_check_mark: Adding tests. Tests when the product doesn't has an Information table, but doesn't specify the dimensions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(asin: "B002QYW8LW",
+Product.create(asin:          "B002QYW8LW",
                category_name: "Baby > Health & Baby Care > Baby Grooming > Brushes & Combs",
-               category_url: "https://www.amazon.ca/b/?node=4624252011",
-               rank: "118",
-               dimensions: "4.3 x 0.4 x 7.9 inches")
+               category_url:  "https://www.amazon.ca/b/?node=4624252011",
+               rank:          "118",
+               dimensions:    "4.3 x 0.4 x 7.9 inches")
+
+Product.create(asin:          "B07HCQV1BT",
+               category_name: "Electronics > Televisions & Video > Televisions",
+               category_url:  "https://www.amazon.ca/b/?node=2690978011",
+               rank:          "142",
+               dimensions:    "")
