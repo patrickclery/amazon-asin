3f99972fe8680edf536e5806ee2264facfeb3aa3
patrickclery <patrick.clery@gmail.com>
2020-03-23 10:44:58 -0700

    :recycle: Refactoring code. Made fetch_product_data.rb thinner and removed tests that were covered by extract_data_from_html_spec.rb

--- spec/services/fetch_product_data_spec.rb
+++ spec/services/fetch_product_data_spec.rb
@@ -2,20 +2,10 @@
 
 RSpec.describe FetchProductData, type: :service do
   subject { described_class.call(asin) }
-
-  # Stub out live requests
-  before(:each) do
-    stub_request(:get, "https://www.amazon.ca/dp/#{asin}")
-      .to_return(body: html)
-  end
-
-  # Eager load these or else the stub will have a different value
-  let!(:asin) { AmazonAsin.new("B002QYW8LW") }
-  # Saved an example product page in /spec/fixtures
-  let!(:html) { File.read(File.join(Rails.root, "/spec/fixtures/amazon-baby-banana.html")) }
-  let!(:expected_response) do
+  let(:expected_response) do
     {
       asin:          "B002QYW8LW",
+      product_title: "Baby Banana Bendable Training Toothbrush (Infant)",
       category_name: "Baby > Health & Baby Care > Baby Grooming > Brushes & Combs",
       category_url:  "https://www.amazon.ca/b/?node=4624252011",
       rank:          "1",
@@ -23,6 +13,23 @@ RSpec.describe FetchProductData, type: :service do
     }
   end
 
-  it { should eq expected_response }
   it { expect(described_class).to respond_to(:call).with(1).arguments }
+
+  context "Valid ASIN" do
+    # Stub out live requests and ensure that the parser doesn't fail
+    before do
+      stub_request(:get, "https://www.amazon.ca/dp/#{asin}")
+      allow(ExtractDataFromHtml)
+        .to receive(:call)
+        .and_return(expected_response)
+    end
+    let(:asin) { "B002QYW8LW" }
+    it { expect(subject).to eq expected_response }
+  end
+
+  context "Invalid ASIN" do
+    # Eager load these or else the stub will have a different value
+    let(:asin) { "b0gus" }
+    it { expect { subject }.to raise_error "Invalid ASIN" }
+  end
 end
